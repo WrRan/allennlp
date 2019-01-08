b91e15de3169539d63acd02e18751af509c16472
WrRan <wrran@outlook.com>
2019-01-08 11:11:41 +0800

    never split some texts

--- allennlp/data/tokenizers/word_splitter.py
+++ allennlp/data/tokenizers/word_splitter.py
@@ -189,8 +189,10 @@ class BertBasicWordSplitter(WordSplitter):
     This is used to split a sentence into words.
     Then the ``BertTokenIndexer`` converts each word into wordpieces.
     """
-    def __init__(self, do_lower_case: bool = True) -> None:
-        self.basic_tokenizer = BertTokenizer(do_lower_case)
+    def __init__(self,
+                 do_lower_case: bool = True,
+                 never_split: List[str] = ("[UNK]", "[SEP]", "[PAD]", "[CLS]", "[MASK]")) -> None:
+        self.basic_tokenizer = BertTokenizer(do_lower_case, never_split)
 
     @overrides
     def split_words(self, sentence: str) -> List[Token]:
